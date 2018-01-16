aa3165bc7605f7fb0cf768ef401335e270e8888c
louise.lapain01 <louise.lapain01@stclairconnect.ca>
2018-01-15 19:18:12 -0500

    in-class assignment

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+  $("#btn").on("click", function(){console.log("button has been clicked")});
+    // If you see ready in the console log of the browser, you have connected the js file correctly.
+
+    // https://www.w3schools.com/jquery/event_click.asp
+});
